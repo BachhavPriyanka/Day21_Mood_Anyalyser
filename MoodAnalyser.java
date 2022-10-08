e5ce117e4de61dbeb3fe72fa06fc14d1925a6c32
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-10-09 02:39:58 +0530

    Handling exception to analays mood is happy or sad

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,10 +1,26 @@
 package day2121;
 
+
 public class MoodAnalyser {
 
-	public String analyseability(String message) {
-		if (message.contains("Sad"))
-			return "SAD";
-		return "HAPPY";
+	String message;
+
+	public MoodAnalyser() {
+		super();
+	}
+
+	public MoodAnalyser(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String analyseability() {
+		try {
+			if (message.contains("Sad"))
+				return "SAD";
+			return "HAPPY";
+		} catch (NullPointerException e) {
+			return "HAPPY";
+		}
 	}
 }
