9a65c254e71bbaf6137a30f0cb1c3c399cd35c62
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-10-09 02:43:28 +0530

    Custom Handling exception to analays mood is happy or sad

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -1,8 +1,11 @@
 package day2121;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.customexp.MoodAnalysisException;
 
 public class MoodAnalyserTest {
 
@@ -11,18 +14,49 @@ public class MoodAnalyserTest {
 	@Test
 	public void testAnalyseAbilityMethod_thenAssertionHappy() {
 		mood = new MoodAnalyser("I am in Happy Mood");
-		assertEquals("HAPPY", mood.analyseability());
+		try {
+			assertEquals("HAPPY", MoodAnalyser.analyseability());
+		} catch (MoodAnalysisException e) {
+		}
 	}
 
 	@Test
 	public void testAnalyseAbilityMethod_thenAssertionSad() {
 		mood = new MoodAnalyser("I am in Sad Mood");
-		assertEquals("SAD", mood.analyseability());
+		try {
+			assertEquals("SAD", MoodAnalyser.analyseability());
+		} catch (MoodAnalysisException e) {
+		}
+	}	
+		
+	@Test
+	public void testgivenNullMoodShouldReturnHaappy_thenAssertionHappy() {
+		mood = new MoodAnalyser(null);
+		try {
+			assertEquals("HAPPY", MoodAnalyser.analyseability());
+		} catch (MoodAnalysisException e) {
+		}
 	}
 	
 	@Test
-	public void testgivenNullMoodShouldReturnHaappy_thenAssertionHappy() {
-		mood = new MoodAnalyser();
-		assertEquals("HAPPY", mood.analyseability());
+	public void testgivenNullMoodShouldThrowMoodAnalysis_thenAssertionHappy() {
+		mood = new MoodAnalyser(null);
+		try {
+			MoodAnalyser.analyseability();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals("Please enter proper message",e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testgivenEmptyMoodShouldThrowMoodAnalysis_thenAssertionHappy() {
+		mood = new MoodAnalyser(" ");
+		try {
+			MoodAnalyser.analyseability();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals("Please enter other than empty message",e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
